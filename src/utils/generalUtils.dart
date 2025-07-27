c3cddf385e4c6af678afc15f32c537c9eeca09ff
Rhian Vinícius <rianresende497@gmail.com>
2025-07-27 19:14:18 -0300

    implement function to remove diaticrits

--- src/utils/generalUtils.dart
+++ src/utils/generalUtils.dart
@@ -1,3 +1,14 @@
 String line(int length, String colorcode, {String character="="}) {
   return '\x1B[${colorcode}m${character*length}\x1B[m';
 }
+
+
+String removeDiaticrits(String text) {
+  const diaticrits = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇ';
+  const normal = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+
+  for (int i = 0; i < diaticrits.length; i++) {
+    text = text.replaceAll(diaticrits[i], normal[i]);
+  }
+  return text;
+}
