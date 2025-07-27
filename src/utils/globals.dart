727a92bbb91cec165d4c990ca9e20115632d5b4d
Rhian Vinícius <rianresende497@gmail.com>
2025-07-27 12:17:11 -0300

    create globals file to define print-lines

---
+++ src/utils/globals.dart
@@ -0,0 +1,9 @@
+import 'generalUtils.dart';
+
+int lineLength = 46;
+
+final String mainMenuLine = line(lineLength, '36');
+final String addMenuLine = line(lineLength, '32');
+final String editMenuLine = line(lineLength, '33');
+final String deleteMenuLine = line(lineLength, '31');
+final String reportMenuLine = line(lineLength, '34');
