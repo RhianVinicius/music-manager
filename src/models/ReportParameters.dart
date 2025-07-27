5c49edb1d821cde7e740ede96062d09f735afe35
Rhian Vinícius <rianresende497@gmail.com>
2025-07-27 11:37:11 -0300

    refactor availible parameters on ReportParameters to be global

--- src/models/ReportParameters.dart
+++ src/models/ReportParameters.dart
@@ -1,11 +1,15 @@
+const List<String> availibleInstrumentParameters = ['Violão', 'Piano', 'sem filtro'];
+const List<String> availibleDificultParameters = ['Fácil', 'Intermediário', 'Difícil', 'sem filtro'];
+const List<String> availibleisLearnedParameters = ['Aprendido', 'Não Aprendido', 'sem filtro'];
+const List<String> availibleMaestryParameters = ['Ruim', 'Mediano', 'Bom', 'sem filtro'];
+const String _exceptionMessage = 'Invalid parameter submitted on ReportParameters model class';
+
 class ReportParameters {
   String _instrument = 'sem filtro';
   String _dificult = 'sem filtro';
   String _isLearned = 'sem filtro';
   String _maestry = 'sem filtro';
-  final String _exceptionMessage = 'Invalid parameter submitted on ReportParameters model class';
-
-
+  
   String get instrument => _instrument;
 
   String get dificult => _dificult;
@@ -14,10 +18,8 @@ class ReportParameters {
 
   String get maestry => _maestry;
 
-
   set instrument(String newParameter) {
-    List<String> availibleParameters = ['Violão', 'Piano', 'sem filtro'];
-    if (availibleParameters.contains(newParameter)) {
+    if (availibleInstrumentParameters.contains(newParameter)) {
       _instrument = newParameter;
     } else {
       throw Exception(_exceptionMessage);
@@ -25,8 +27,7 @@ class ReportParameters {
   }
 
   set dificult(String newParameter) {
-    List<String> availibleParameters = ['Fácil', 'Intermediário', 'Difícil', 'sem filtro'];
-    if (availibleParameters.contains(newParameter)) {
+    if (availibleDificultParameters.contains(newParameter)) {
       _dificult = newParameter;
     } else {
       throw Exception(_exceptionMessage);
@@ -34,8 +35,7 @@ class ReportParameters {
   }
 
   set isLearned(String newParameter) {
-    List<String> availibleParameters = ['Aprendido', 'Não Aprendido', 'sem filtro'];
-    if (availibleParameters.contains(newParameter)) {
+    if (availibleisLearnedParameters.contains(newParameter)) {
       _isLearned = newParameter;
     } else {
       throw Exception(_exceptionMessage);
@@ -43,14 +43,12 @@ class ReportParameters {
   }
 
   set maestry(String newParameter) {
-    List<String> availibleParameters = ['Ruim', 'Mediano', 'Bom', 'sem filtro'];
-    if (availibleParameters.contains(newParameter)) {
+    if (availibleMaestryParameters.contains(newParameter)) {
       _maestry = newParameter;
     } else {
       throw Exception(_exceptionMessage);
     }
   }
-
 
   ReportParameters();
 }
