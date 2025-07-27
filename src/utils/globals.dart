5a52b5e6247d9ea925f6066903611b49d045fbe3
Rhian Vinícius <rianresende497@gmail.com>
2025-07-27 18:39:12 -0300

    add global-options

--- src/utils/globals.dart
+++ src/utils/globals.dart
@@ -7,3 +7,9 @@ final String addMenuLine = line(lineLength, '32');
 final String editMenuLine = line(lineLength, '33');
 final String deleteMenuLine = line(lineLength, '31');
 final String reportMenuLine = line(lineLength, '34');
+
+
+const List<String> instrumentOptions = ['Violão', 'Piano'];
+const List<String> dificultOptions = ['Fácil', 'Intermediário', 'Difícil'];
+const List<String> isLearnedOptions = ['Aprendido', 'Não Aprendido'];
+const List<String> maestryOptions = ['Indefinido', 'Ruim', 'Mediano', 'Bom'];
